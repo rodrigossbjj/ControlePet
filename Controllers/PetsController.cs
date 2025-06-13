9da6bc1dc60e28ce857841b7b1e08a792020937d
Murilo Tamura <murilotamura@gmail.com>
2025-06-13 16:55:47 -0300

    Novas controllers, TutoresController e PetsController
    Criação das controllers de tutores e pets

---
+++ Controllers/PetsController.cs
@@ -0,0 +1,41 @@
+﻿using ControlePetWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlePetWeb.Controllers
+{
+    public class PetsController : Controller
+    {
+        private readonly Context _context;
+
+        public PetsController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var pets = _context.Pets.Include(p => p.Tutor).ToList();
+            return View(pets);
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Tutores = _context.Tutores.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Pets.Add(pet);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Tutores = _context.Tutores.ToList();
+            return View(pet);
+        }
+    }
+}
---
+++ Controllers/TutoresController.cs
@@ -0,0 +1,38 @@
+﻿using ControlePetWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControlePetWeb.Controllers
+{
+    public class TutoresController : Controller
+    {
+        private readonly Context _context;
+
+        public TutoresController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var tutores = _context.Tutores.ToList();
+            return View(tutores);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Tutor tutor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tutores.Add(tutor);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tutor);
+        }
+    }
+}
