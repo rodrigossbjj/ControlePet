ae6a2fc4240e5a2cdb88482aedadd1e5963419e9
SafeiRodrigo <141763679+SafeiRodrigo@users.noreply.github.com>
2025-06-19 21:47:30 -0300

    Model de consulta para Pets
    Model novo de entidade Consulta

---
+++ Models/Consulta.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControlePetWeb.Models
+{
+    public class Consulta
+    {
+        public int Con_Id { get; set; }
+
+        [Required]
+        public int Con_PetId { get; set; }
+
+        [Required]
+        public DateTime Con_Data { get; set; }
+
+        public string Con_Descricao { get; set; }
+
+        public string Con_Status { get; set; } //Ex: "Agendada", "Realizada", "Cancelada"
+
+        [ValidateNever]
+        public Pet Pet { get; set; }
+    }
+
+}
